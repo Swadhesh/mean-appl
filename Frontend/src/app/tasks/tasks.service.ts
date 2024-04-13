4c948e68e56d2bbf87e80914e2b54f8f919a8315
Swadhesh P U <75163426+Swadhesh@users.noreply.github.com>
2024-04-13 13:05:09 +0530

    changes

--- Frontend/src/app/tasks/tasks.service.ts
+++ Frontend/src/app/tasks/tasks.service.ts
@@ -14,27 +14,27 @@ export class TasksService {
 
   //retreiving tasks
   getTasks(){
-    return this.http.get('http://ec2-34-195-133-61.compute-1.amazonaws.com:3200/api/tasks').pipe(map(res=>res));
+    return this.http.get('http://ec2-3-215-68-245.compute-1.amazonaws.com:3200/api/tasks').pipe(map(res=>res));
   }
 
   addTask(newTask:any){
     var headers=new HttpHeaders();
     //const headers = new HttpHeaders().set('Content-Type', 'application/json')
     headers.append('Content-Type','application/json');
-    return this.http.post('http://ec2-34-195-133-61.compute-1.amazonaws.com:3200/api/add',newTask,{headers:headers}).pipe(map(res=>res));
+    return this.http.post('http://ec2-3-215-68-245.compute-1.amazonaws.com3200/api/add',newTask,{headers:headers}).pipe(map(res=>res));
   }
   deleteTask(id:any){
-    return this.http.delete('http://ec2-34-195-133-61.compute-1.amazonaws.com:3200/api/delete/'+id).pipe(map(res => res));
+    return this.http.delete('http://ec2-3-215-68-245.compute-1.amazonaws.com:3200/api/delete/'+id).pipe(map(res => res));
   }
 
   viewTask(id:any){
-    return this.http.get('http://ec2-34-195-133-61.compute-1.amazonaws.com:3200/api/viewtask/'+id).pipe(map(res => res));
+    return this.http.get('http://ec2-3-215-68-245.compute-1.amazonaws.com:3200/api/viewtask/'+id).pipe(map(res => res));
   }
 
   updateTask(id:any,newTask:any){
     var headers=new HttpHeaders();
     headers.append('Content-Type','application/json');
-    return this.http.post('http://ec2-34-195-133-61.compute-1.amazonaws.com:3200/api/update/'+id,newTask,{headers:headers}).pipe(map(res=>res));
+    return this.http.post('http://ec2-3-215-68-245.compute-1.amazonaws.com:3200/api/update/'+id,newTask,{headers:headers}).pipe(map(res=>res));
   }
 
 }
